d0f989f9ec7c9f1e5f4336738b4aa03ad1d61bc0
roman <s91630277@gmail.com>
2020-03-24 23:45:13 +0400

    chore: Working variant

--- plugin.js
+++ plugin.js
@@ -1,14 +1,2 @@
 const _ = require('lodash');
-
-module.exports = () => {
-    return ({ addUtilities, addComponents, addVariant, e, theme }) => {
-        addVariant('ie', ({ container, separator }) => {
-            const supportsRule = postcss.atRule({ name: 'supports', params: '(display: grid)' });
-            supportsRule.append(container.nodes);
-            container.append(supportsRule);
-            supportsRule.walkRules((rule) => {
-                rule.selector = `.${e(`supports-grid${separator}${rule.selector.slice(1)}`)}`;
-            });
-        });
-    };
-};
+const postcss = require('postcss');
